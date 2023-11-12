bc90da735a6aef93fbc42ec28337b89abd2e6695
Venkatesan-M <venkygaming18@gmail.com>
2023-11-13 02:05:39 +0530

    Upgrade Pre-Loader UX

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
 import "./style.css";
 import Experience from "./Experience/Experience.js";
+import Facts from "./Facts.js";
+import { shuffleArray } from "./Facts.js";
 
 const experience = new Experience(document.querySelector(".experience-canvas"));
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var factPlaceholder = document.getElementById("fact");
+    var factNumber;
+ 
+    function randomFact(){
+        shuffleArray(Facts)
+      return Facts[factNumber];
+    }
+  
+    factNumber = Math.floor(Math.random() * Facts.length);
+    factPlaceholder.textContent = randomFact();
+
+  });
